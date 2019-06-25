8e4fdea30eb424a316042db0de8c996df4bb8544
Ryan Weingart <rmweingart5@gmail.com>
2019-06-25 12:32:44 -0400

    added on click events to buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,3 +6,31 @@ var darkBlueCrystal = "";
 var redCrystal = "";
 var wins = 0;
 var losses = 0;
+
+
+
+$(".blue").on("click", function () {
+    alert('blue');
+});
+
+$(".orange").on("click", function () {
+    alert('orange');
+});
+
+$(".darkBlue").on("click", function () {
+    alert('dark blue');
+});
+
+$(".red").on("click", function () {
+    alert('red');
+});
+
+function gameStarter () {
+
+
+}
+
+function endGame () {
+
+
+}
