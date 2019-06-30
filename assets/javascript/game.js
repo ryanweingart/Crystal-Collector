89737e6b5ae706a0e8a9501d4b742636bdcef8cc
Ryan Weingart <rmweingart5@gmail.com>
2019-06-30 13:31:38 -0400

    added code descriptions to javaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+//Global variables
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
 var log = console.log;
 var randomNumber = 0;
 var playerScore = 0;
@@ -7,6 +10,9 @@ var darkBlueCrystal = 0;
 var redCrystal = 0;
 var wins = 0;
 var losses = 0;
+
+//Function that starts game with new random number and player score back to 0.
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
 function gameStarter () {
     playerScore = 0;
@@ -22,6 +28,8 @@ function gameStarter () {
     $("#playerScore-text").text(playerScore);
 }
 
+//Button click functions that add value of button to player score and calls endGame function to check if game has been won
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 $("#blue").on("click", function () {
     playerScore += blueCrystal;
     endGame ();
@@ -46,6 +54,8 @@ $("#red").on("click", function () {
     $("#playerScore-text").text(playerScore);
 });
 
+//Function that checks if player score has matched random number to win or gone over random number to lose
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 function endGame () {
 
     if (playerScore === randomNumber) {
