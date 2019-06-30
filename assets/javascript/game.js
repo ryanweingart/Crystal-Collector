5fe6954f31e6d5804d21abf27eaad15043e6dd46
Ryan Weingart <rmweingart5@gmail.com>
2019-06-30 13:24:11 -0400

    cleaned up code layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,6 @@ var redCrystal = 0;
 var wins = 0;
 var losses = 0;
 
-
-
 function gameStarter () {
     playerScore = 0;
 
@@ -23,46 +21,40 @@ function gameStarter () {
     $("#randomNumber-text").text(randomNumber);
     $("#playerScore-text").text(playerScore);
 }
+
 $("#blue").on("click", function () {
     playerScore += blueCrystal;
     endGame ();
-  
     $("#playerScore-text").text(playerScore);
 });
 
 $("#orange").on("click", function () {
     playerScore += orangeCrystal;
     endGame ();
-
     $("#playerScore-text").text(playerScore);
-
 });
 
 $("#darkBlue").on("click", function () {
     playerScore += darkBlueCrystal;
     endGame ();
-
     $("#playerScore-text").text(playerScore);
-
 });
 
 $("#red").on("click", function () {
     playerScore += redCrystal;
     endGame ();
-
     $("#playerScore-text").text(playerScore);
-
 });
 
 function endGame () {
 
-   
     if (playerScore === randomNumber) {
         alert("You win!");
         wins++;
         $("#wins-text").text(wins);
 
         gameStarter ();
+
     } else if (playerScore > randomNumber) {
         alert("You lose!");
         losses++;
@@ -70,10 +62,7 @@ function endGame () {
 
         gameStarter ();
     }
-
-
 }
-
 
 gameStarter ();
 
